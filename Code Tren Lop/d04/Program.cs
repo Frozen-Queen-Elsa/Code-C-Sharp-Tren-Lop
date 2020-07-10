2ad667764885e97a7b085558fd0d0cbffca78538
IRIS <bebebietyeu0903@gmail.com>
2020-07-10 12:06:24 +0700

    Add files via upload

--- Code Tren Lop/d04/Program.cs
+++ Code Tren Lop/d04/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Banking;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +10,52 @@ namespace d04
     class Program
     {
         static void Main(string[] args)
+        {
+            Program pro = new Program();
+            pro.Menu();
+
+            // pro.Demo();
+
+
+            Console.ReadKey();
+        }
+
+        void Menu()
+        {
+            ATMAccount atmACC = new ATMAccount();
+            int amt = 0;
+            string op = "";
+            while (true)
+            {
+                
+                Console.Clear();
+                Console.WriteLine("MENU");
+                Console.WriteLine("1. Rut tien");
+                Console.WriteLine("2. Nap tien");
+                Console.WriteLine("3. Xem so du");
+                Console.WriteLine("4. Thoat");
+                op = Console.ReadLine().Trim();
+                switch (op)
+                {
+                    case "1":
+                        Console.WriteLine("Nhap so tien muon rut:");
+                        amt = int.Parse(Console.ReadLine().Trim());
+                        atmACC.pWithdraw = amt;
+                        break;
+                    case "2":
+                        Console.WriteLine("Nhap so tien muon rut:");
+                        amt = int.Parse(Console.ReadLine().Trim());
+                        atmACC.pDeposit = amt;
+                        break;
+                    case "3":
+                        Console.WriteLine("{0}",atmACC);
+                        break;
+                }
+            }
+        }
+
+
+        void Demo()
         {
             Student sv = new Student();
 
@@ -48,14 +95,11 @@ namespace d04
             Console.WriteLine();
             Console.WriteLine("Demo Indexer");
             Numbers oNum = new Numbers();
-            Console.WriteLine("Phan tu cuoi : {0}", oNum[oNum.pLen-1]);
+            Console.WriteLine("Phan tu cuoi : {0}", oNum[oNum.pLen - 1]);
 
             //Gan gia tri cho phan tu dau tien 
             oNum[0] = 13;
             Console.WriteLine("Phan tu dau tien : {0}", oNum[0]);
-
-
-            Console.ReadKey();
         }
     }
 }

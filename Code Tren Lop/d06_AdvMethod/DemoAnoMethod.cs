ee9b55a975da1d8ec0815fe1e457b999eb98c398
IRIS <bebebietyeu0903@gmail.com>
2020-07-15 12:12:14 +0700

    Add files via upload

---
+++ Code Tren Lop/d06_AdvMethod/DemoAnoMethod.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace d06_AdvMethod
+{
+    public delegate void DCasio(int a, int b);
+    public class DemoAnoMethod
+    {
+        public void test() {
+            DCasio casio = delegate (int x, int y)
+            {
+                int r = x + y;
+                Console.WriteLine($"{x} + {y} = {r}");
+            };
+
+            casio += delegate (int x, int y)
+            {
+                int r = x * y;
+                Console.WriteLine($"{x} * {y} = {r}");
+            };
+
+            casio(10, 4);
+        }
+    }
+
+    public static class DemoExtensionMethod
+    {
+        public static bool ContainIgnocase(this string s, string subs)
+        {
+            if (Regex.IsMatch(s, subs, RegexOptions.IgnoreCase)) {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Code Tren Lop/d06_AdvMethod/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace d06_AdvMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***********************ANONYMOUS METHOD***********************");
+            DemoAnoMethod d1 = new DemoAnoMethod();
+            d1.test();
+
+            Console.WriteLine("***********************EXTENSION METHOD***********************");
+            Console.Write("Nhap ho ten cua ban: ");
+            string hoten = Console.ReadLine().Trim();
+            Console.Write("Nhap chuoi ki tu bat ki: ");
+            string m = Console.ReadLine().Trim();
+            if (hoten.ContainIgnocase(m))
+            {
+                Console.WriteLine($"[{m}] co xuat hien trong [{hoten}]");
+            }
+            else {
+                Console.WriteLine("Khong tim thay");
+            }
+
+        }
+    }
+}
